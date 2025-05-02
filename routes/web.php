3353d2dbd3dbbd5dd9b1d751d3e59ff16c562ebe
its_hious <awukusamuel20@gmail.com>
2025-05-02 19:00:10 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Website\WebsiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[WebsiteController::class,'index'])->name('/');
-
 Route::get('/about',[WebsiteController::class,'about'])->name('about');
 Route::get('/services',[WebsiteController::class,'services'])->name('services');
 Route::get('/contact',[WebsiteController::class,'contact'])->name('contact');
